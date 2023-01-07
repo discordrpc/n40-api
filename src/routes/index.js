76245fd053ac1ed46de8574e4c215803aea811a0
DiscordRPC <lukashammerprivate@gmail.com>
2023-01-07 12:01:23 +0000

    feat: Implement body-parser

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,15 @@
 const fs = require('fs');
+const bodyParser = require('body-parser');
 
 module.exports = (app) => {
+  // Saves a valid JSON body to req.rawBody
+  app.use(bodyParser.json({
+    verify: (req, res, buf, enc) => {
+      if (buf && buf.length) req.rawBody = buf.toString(enc, 'utf8');
+    }
+  }));
+
+  // Load all routes
   fs.readdirSync(__dirname).forEach(file => {
     if (file === 'index.js') return;
     if (!file.endsWith('.js')) return;
