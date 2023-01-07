262faab8b9d967386158c8844c5adb3e41f517f6
DiscordRPC <lukashammerprivate@gmail.com>
2023-01-07 12:01:10 +0000

    fix: Commits deploy successfully

--- src/routes/deploy.js
+++ src/routes/deploy.js
@@ -1,39 +1,49 @@
-const { exec } = require('child_process');
+const { promisify } = require('util');
+const exec = promisify(require('child_process').exec);
 const pm2 = require('pm2');
 const crypto = require('crypto');
 
 module.exports = {
   method: 'POST',
   path: '/deploy',
-  handler: (req, res) => {
+  middleware: [],
+  handler: async (req, res) => {
+    // Check if a raw body exists
+    if (!req.rawBody) return res.status(400).json({ error: 'invalid body' });
+    
     // Get request signature
     const sig = Buffer.from(req.get('X-Hub-Signature-256') || '', 'utf8');
     // Create digest from request body
     const hmac = crypto.createHmac('sha256', process.env.DEPLOY_SECRET);
-    const digest = Buffer.from('sha256=' + hmac.update(JSON.stringify(req.body) || '').digest('hex'), 'utf8');
+    const digest = Buffer.from('sha256=' + hmac.update(req.rawBody).digest('hex'), 'utf8');
 
     // Compare generated digest to the request signature
     if (sig.length != digest.length || crypto.timingSafeEqual(sig, digest))
-      return res.status(400).json({ error: 'invalid token' });
+      return res.status(401).json({ error: 'invalid token' });
 
     // Execute git pull
-    exec('git pull', (err, _, stderr) => {
-      if (err || stderr) {
-        console.log(err || stderr);
-        res.status(500).json({ error: 'failed to pull' });
+    try {
+      let { stdout, stderr } = await exec('git pull');
+      if (stdout) console.log(stdout);
+      if (stderr) {
+        console.log(stderr);
+        return res.status(500).json({ error: 'git pull failed' });
       }
 
-      // Restart the server
-      pm2.connect((err) => {
-        if (err) throw err;
+      // Restart the PM2 process
+      pm2.connect(err => {
+        if (err) throw (err);
 
-        pm2.restart('HTTP-API', (err) => {
-          if (err) throw err;
+        pm2.restart(process.env.pm_id, err => {
+          if (err) throw (err);
         });
       });
-    });
 
-    // Send success response
-    res.status(200).json({ success: 'deployed' });
+      // Return success status
+      return res.status(200).json({ success: 'changes applied' });
+    } catch (e) {
+      console.error(e);
+      return res.status(500).json({ error: 'git pull failed' });
+    }
   }
 }
