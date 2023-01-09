7603fa173749082d161ece140c4c4d8bf73ce0cf
DiscordRPC <lukashammerprivate@gmail.com>
2023-01-09 22:59:38 +0000

    Add auto-build & auto-update dependencies

--- scripts/dev-pull.sh
+++ scripts/dev-pull.sh
@@ -3,3 +3,9 @@
 # Pulls the latest commit from the dev branch
 git fetch origin dev
 git reset --hard origin/dev
+
+# Install or update dependencies
+npm install
+
+# Builds the latest commit
+npm run build
--- scripts/prod-pull.sh
+++ scripts/prod-pull.sh
@@ -3,3 +3,9 @@
 # Pulls the latest commit from the production branch
 git fetch origin master
 get reset --hard origin/master
+
+# Install or update dependencies
+npm install
+
+# Builds the latest commit
+npm run build
